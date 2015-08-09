3f7c7641d678825cdb0d7dfa0668ac1a2a447c38
Li-Wen Hsu <lwhsu@lwhsu.org>
2015-08-10 02:53:22 +0800

    do thing that does not need network

--- jobs/test-lwhsu-build-in-jail/build.sh
+++ jobs/test-lwhsu-build-in-jail/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -x
 
-printf "GET / HTTP/1.0\nHost: www.freebsd.org\n\n" | nc -w 3 www.freebsd.org 80 | grep '200 OK'
-echo $?
+#printf "GET / HTTP/1.0\nHost: www.freebsd.org\n\n" | nc -w 3 www.freebsd.org 80 | grep '200 OK'
+#echo $?
+
+echo "hello world"
